4af48f6e97b093d2c18274c08874f411a513fa0c
SalindaHulangamuwa <150468057+SalindaHulangamuwa@users.noreply.github.com>
2025-02-18 07:01:45 +0530

    add a summarizer

--- storeApi/main.py
+++ storeApi/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from storeApi.routers.post import router as post_router
 from storeApi.routers.comment import router as comment_router
+from storeApi.routers.summarize import router as summarize_router
 app=FastAPI()
 
 app.include_router(post_router, prefix="/posts")
 app.include_router(comment_router, prefix="/comments")
+app.include_router(summarize_router, prefix="/summarizes")
 
